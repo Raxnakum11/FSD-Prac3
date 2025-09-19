9654d4679bfaf5d6d1f36552fc3d36af915ad240
Raxnakum11 <148608416+Raxnakum11@users.noreply.github.com>
2025-09-19 21:54:41 +0530

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import WelcomeClock from "./Components/WelcomeClock";
+import CounterApp from './Components/CounterApp.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <WelcomeClock />
+    {/* <WelcomeClock /> */}
+    <CounterApp />
   </StrictMode>,
 )
